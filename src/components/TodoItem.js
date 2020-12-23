90cfda70af0b06f949dd144f9c7aa655e3c93d38
lauraharston <lauraharston@gmail.com>
2020-12-23 12:13:31 -0600

    created onClick method to toggle checkbox ofeach todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function TodoItem(props) {
+    
+    const handleChange = () => console.log('change')
     return(
         <div className='todo-item'>
-            <input type="checkbox" checked={props.item.completed}></input>
+            <input type="checkbox" 
+            checked={props.item.completed} 
+            onChange={() => props.handleChange(props.item.id)}
+
+            />
             <p>{props.item.text}</p>   
         </div>
     )
